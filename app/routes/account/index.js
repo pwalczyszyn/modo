62ba7614e0d6b12ba49dd170d1cbd4912de41821
Piotr Walczyszyn <piotr.walczyszyn@gmail.com>
2014-10-26 21:55:24 +0100

    auth wip

---
+++ app/routes/account/index.js
@@ -0,0 +1,3 @@
+'use strict';
+
+exports.password = require('./password');
--- config/routes.js
+++ config/routes.js
@@ -3,7 +3,9 @@
 var express = require('express');
 var wwwRouter = express.Router();
 var authRouter = express.Router();
+var accountRouter = express.Router();
 var gmail = require('app/routes/authorize/gmail');
+var account = require('app/routes/account');
 
 module.exports = function(app) {
 
@@ -14,4 +16,9 @@ module.exports = function(app) {
   authRouter.get('/gmail/callback', gmail.callback);
   app.use('/authorize', authRouter);
 
+  accountRouter.get('/password', account.password.request);
+  accountRouter.post('/password', account.password.request);
+  accountRouter.get('/password/:passwordCode', account.password.set);
+  accountRouter.post('/password:passwordCode', account.password.set);
+  app.user('/account', accountRouter);
 };
