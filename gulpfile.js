6895ce55ea656f20f79c68724e6a4bfb621d44da
Piotr Walczyszyn <piotr.walczyszyn@gmail.com>
2014-10-29 18:59:22 +0100

    initial work on adding gulp

---
+++ gulpfile.js
@@ -0,0 +1,10 @@
+'use strict';
+
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var notify = require('gulp-notify');
+var autoprefixer = require('gulp-autoprefixer');
+
+gulp.task('styles', function() {
+  return gulp.src('');
+});
